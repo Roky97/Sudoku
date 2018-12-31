afb11aff9adc27c1bc1caac2322fa1dfaa3a24fe
RokyMac <RokyMac@10.37.129.2>
2018-12-31 17:22:31 +0100

    Added main

---
+++ SudokuINGSW/src/main/java/gui/application/main.java
@@ -0,0 +1,24 @@
+package gui.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import gui.view.ViewManager;
+
+public class main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			ViewManager manager = new ViewManager();
+			primaryStage = manager.getStage();
+		
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
